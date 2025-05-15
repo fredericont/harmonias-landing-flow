a3241ebde26c7f5aac70843ef59342a810da2ee8
Frederico <aspdeassis@gmail.com>
2025-05-15 15:36:33 -0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const repositoryName = "harmonias-landing-flow"; // troque se necessário
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repositoryName}/`,  // 👈 linha necessária para funcionar no GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
